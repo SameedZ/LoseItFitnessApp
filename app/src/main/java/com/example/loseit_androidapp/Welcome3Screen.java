3a0e9cdf92838857daea3118db3bb18b274b03d6
Sameed Zahoor <113443310+SameedZ@users.noreply.github.com>
2022-11-23 14:39:12 +0500

    Login Screen Added

--- app/src/main/java/com/example/loseit_androidapp/Welcome3Screen.java
+++ app/src/main/java/com/example/loseit_androidapp/Welcome3Screen.java
@@ -2,13 +2,36 @@ package com.example.loseit_androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Welcome3Screen extends AppCompatActivity {
+
+    TextView tv_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome3_screen);
+
+        tv_next = findViewById(R.id.tv_next);
+        tv_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome3Screen.this,LoginScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
+
+
+
+
+
+
 }
