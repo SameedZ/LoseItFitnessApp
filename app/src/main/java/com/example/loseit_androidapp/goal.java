1308dd577590840d544e3815e465f2207ef18d21
SyedMustafa145 <i190544@nu.edu.pk>
2022-12-02 23:06:55 +0500

    ui screens continued

---
+++ app/src/main/java/com/example/loseit_androidapp/goal.java
@@ -0,0 +1,14 @@
+package com.example.loseit_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class goal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal);
+    }
+}
---
+++ app/src/main/java/com/example/loseit_androidapp/weightgoal.java
@@ -0,0 +1,14 @@
+package com.example.loseit_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weightgoal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weightgoal);
+    }
+}
