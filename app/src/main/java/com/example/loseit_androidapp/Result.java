ef97e6873f6b42f3f226adb7e0de5425c92bed25
ArmishMunir <armish.muneer@gmail.com>
2022-12-04 02:59:09 +0500

    result activity

---
+++ app/src/main/java/com/example/loseit_androidapp/Result.java
@@ -0,0 +1,13 @@
+package com.example.loseit_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
