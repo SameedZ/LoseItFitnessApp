b98b04e15448d0c5b4033d1c828d6cb97ba36b73
ArmishMunir <armish.muneer@gmail.com>
2022-12-04 03:41:40 +0500

    start training activity

---
+++ app/src/main/java/com/example/loseit_androidapp/StartTraining.java
@@ -0,0 +1,13 @@
+package com.example.loseit_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartTraining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_training);
+    }
+}
