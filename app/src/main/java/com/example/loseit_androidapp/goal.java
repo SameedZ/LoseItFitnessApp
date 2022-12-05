9dc06b835a0202a4ca533757a5d1b2cc1d92cdff
SyedMustafa145 <i190544@nu.edu.pk>
2022-12-05 12:46:58 +0500

    Error resolved

--- app/src/main/java/com/example/loseit_androidapp/goal.java
+++ app/src/main/java/com/example/loseit_androidapp/goal.java
@@ -2,13 +2,42 @@ package com.example.loseit_androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class goal extends AppCompatActivity {
+
+    Button btn_skip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
+
+        btn_skip = findViewById(R.id.btn_skip);
+
+        btn_skip.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                movetonextactivity();
+
+            }
+
+            private void movetonextactivity() {
+
+                Intent intent = new Intent(goal.this, getstarted.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
     }
 }
